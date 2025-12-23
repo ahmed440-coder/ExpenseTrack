57e47d63167ced75d0d06ba97ae50c1b04888047
ahmed <ahmedbenhsn123@gmail.com>
2025-12-23 05:40:58 +0100

    layout fixes

--- frontend/src/pages/auth/Signup.jsx
+++ frontend/src/pages/auth/Signup.jsx
@@ -76,7 +76,7 @@ const Signup = () => {
         </p>
         <ProfilePhotoSelector image={profilePic} setImage={setProfilePic} />
         <form onSubmit={handleSignup}>
-          <div className='grid grid-cols-1 md:grid-cols-2 gap-4'>
+          <div className='grid grid-cols-2 md:grid-cols-2 gap-4'>
             <Input
               value={fullName}
               onChange={({target}) => setFullName(target.value)}
