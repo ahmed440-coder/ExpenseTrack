92529f690c419c034c5b9347c7369f31f0fb5808
ahmed <ahmedbenhsn123@gmail.com>
2025-12-23 04:40:42 +0100

    deployment fixes

--- backend/server.js
+++ backend/server.js
@@ -36,7 +36,7 @@ app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
 
 app.get(/.*/, (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
+    res.sendFile(path.join(__dirname, "../frontend", "index.html"));
 });
 
 const PORT = process.env.PORT || 5000;
