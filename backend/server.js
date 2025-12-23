97f0e8154ccfeb7287fd92dd6dc728069ee043a6
ahmed <ahmedbenhsn123@gmail.com>
2025-12-23 04:49:07 +0100

    depoyment fixes

--- backend/server.js
+++ backend/server.js
@@ -71,7 +71,7 @@ app.use("/uploads", express.static(path.join(__dirname, "uploads")))
 
 app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-app.get("/*", (req, res) => {
+app.get(/.*/, (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
 });
 
